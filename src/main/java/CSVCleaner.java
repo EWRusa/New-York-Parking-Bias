37ef92458810732d1624344614781bcc274096d3
EWRusa <stringcheeseinc1@gmail.com>
2023-11-15 18:54:35 -0700

    CSVcleaner

---
+++ src/main/java/CSVCleaner.java
@@ -0,0 +1,12 @@
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
+public final class CSVCleaner {
+    private Dataset<Row> dataset;
+
+    public static void main(String[] args) {
+        
+    }
+
+    //function for going through column and creating numerical equivalents to string values
+}
