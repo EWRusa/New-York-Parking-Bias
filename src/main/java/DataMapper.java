1f72c17020bf983b07220a1f2428a5d267d9061d
EWRusa <stringcheeseinc1@gmail.com>
2023-11-20 12:59:35 -0700

    Broke DataMapper into functions for better formatting and modification

--- src/main/java/DataMapper.java
+++ src/main/java/DataMapper.java
@@ -1,6 +1,10 @@
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.ml.feature.LabeledPoint;
+import org.apache.spark.ml.linalg.DenseVector;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.storage.StorageLevel;
 import scala.Tuple2;
 import scala.collection.immutable.HashMap;
 import scala.collection.immutable.Map;
@@ -8,27 +12,43 @@ import scala.collection.immutable.Map;
 import java.util.Iterator;
 
 public final class DataMapper {
+    static String[] featuresToCapture = {"Vehicle Make", "Vehicle Body Type",
+            "Issuing Agency","Vehicle Expiration Date", "Plate Type", "Street Name", "Intersecting Street"};
 
-    public void columnFixer() {
-        String[] capturedFeatures = {"Vehicle Make", "Vehicle Body Type",
-                "Issuing Agency","Vehicle Expiration Date", "Plate Type", "Street Name", "Intersecting Street"};
+    public static void main(String[] args) {
+        String predictedLabel = "Vehicle Make";
 
         String[] featuresToFix = {"Vehicle Make", "Vehicle Body Type",
                 "Issuing Agency","Vehicle Expiration Date", "Plate Type", "Street Name", "Intersecting Street"};
 
         SparkSession spark = SparkSession
                 .builder()
-                .appName("ClassificationMapper").master("local")
+                .appName("DataMapper").master("local")
                 .getOrCreate();
-        Dataset<Row> dataset = spark.read().option("header", "true").csv("NYC_SAMPLE_DATA.csv");
+        Dataset<Row> dataset = spark.read().option("header", "true")
+                .csv("NYC_SAMPLE_DATA.csv").persist(StorageLevel.MEMORY_AND_DISK());
 
         //UNTESTED but should work to remap values on a dataset
-        dataset.withColumn("Vehicle Make", dataset.col("Vehicle Make").apply(classifierToRDD("Vehicle Make", spark)));
+        for (String feature: featuresToFix) {
+            dataset.withColumn(feature, dataset.col(feature).apply(classifierToRDD(feature, spark)));
+        }
 
+        JavaRDD<LabeledPoint> dataForRandomForest = dataset.toJavaRDD()
+                .map(row -> new LabeledPoint(row.getDouble(row.fieldIndex(predictedLabel)), vectorBuilder(row)));
 
+        dataForRandomForest.saveAsTextFile(String.format("random_forest_dataset_%s",predictedLabel.toLowerCase().replace(" ", "_")));
+        spark.stop();
     }
 
-    public Map<String, Double> classifierToRDD(String columnName, SparkSession spark) {
+    public static DenseVector vectorBuilder(Row row) {
+        double[] vals = new double[featuresToCapture.length];
+
+        for (int i = 0; i < vals.length; i++) vals[i] = row.getDouble(row.fieldIndex(featuresToCapture[i]));
+
+        return new DenseVector(vals);
+    }
+
+    public static Map<String, Double> classifierToRDD(String columnName, SparkSession spark) {
         Dataset<Row> column = spark.read().option("header", "false")
                 .csv(String.format("val_%s", columnName.toLowerCase().replace(" ", "_")));
 
