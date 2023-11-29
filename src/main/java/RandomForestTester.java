2fb7e5fc95b7277d89f6e2f93e77313b278d3699
EWRusa <stringcheeseinc1@gmail.com>
2023-11-29 11:41:15 -0700

    Added accuracy tester for model

--- src/main/java/RandomForestTester.java
+++ src/main/java/RandomForestTester.java
@@ -2,6 +2,7 @@ import org.apache.log4j.Logger;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.ml.classification.RandomForestClassificationModel;
+import org.apache.spark.ml.param.Param;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -12,7 +13,7 @@ public class RandomForestTester {
     public static void main(String[] args) {
         SparkSession spark = SparkSession
                 .builder()
-                .appName("RandomForestTester").master("local")
+                .appName("RandomForestTester").master("java")
                 .getOrCreate();
 
         JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
@@ -33,11 +34,15 @@ public class RandomForestTester {
         AtomicInteger countCorrect = new AtomicInteger();
 
         predictionsUnifiedToActual.foreach((Row row) -> rowCompare(row, countCorrect));
+        double accuracyFor2023 = (double) countCorrect.get() / (double) predictionsUnifiedToActual.count(); //placeholder
 
-        double accuracy = (double) countCorrect.get() / (double) predictionsUnifiedToActual.count(); //placeholder
         Logger logger = Logger.getRootLogger();
 
-        logger.info(String.format("Error for Predicting 2023 %s: %.6f", datapathLabel, 1.0 - accuracy));
+        logger.info(String.format("Error for overall model %s: %.6f", datapathLabel, 1.0 - modelToTest.summary().accuracy()));
+        logger.info(String.format("Error for Predicting 2023 %s: %.6f", datapathLabel, 1.0 - accuracyFor2023));
+
+        Param<String> probabilities = modelToTest.probabilityCol();
+        logger.info(probabilities.toString());
 
         spark.stop();
     }
